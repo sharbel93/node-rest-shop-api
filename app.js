a142653ff2d2088cef51de993a394ed2cf0998e0
sharbel chris <sharbel.chris@gmail.com>
2019-04-02 10:36:50 +0300

    Parsing the body & handling CORS

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//preventing CORS errors
+app.use((req,res, next) =>{
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requeasted-With, Content-Type, Accept, Authorization ');
+   if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+   }
+   next();
+});
+
 // Routes that handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
